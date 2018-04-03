b0030a8c156bdf41f0281eeaa8d1f5ce029f0114
roisinanglim <36383923+roisinanglim@users.noreply.github.com>
2018-04-03 19:14:01 +0100

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -6,7 +6,7 @@ i=2
 while i < n: 
 #Set C as a counter
   c=0  
-    #If N modulus i is equal to count the amount of times it equals zero. If its greater than 2 dont print i.
+    #If N modulus i is equal to zero , count the amount of times it equals zero. If its greater than 2 dont print i.
     if n % i == 0:
         for x in range(1,i): 
              if i % x == 0:
